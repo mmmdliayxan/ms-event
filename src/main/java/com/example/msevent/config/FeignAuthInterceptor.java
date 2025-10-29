65a270f79c8c85227fe215c20f1fc2b34596aa2e
Aykhan Mammadli <mmmdliayxan2107@gmail.com>
2025-10-29 12:46:59 +0400

    change interceptor

--- src/main/java/com/example/msevent/config/FeignAuthInterceptor.java
+++ src/main/java/com/example/msevent/config/FeignAuthInterceptor.java
@@ -8,12 +8,19 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Component
 public class FeignAuthInterceptor implements RequestInterceptor {
+
     @Override
     public void apply(RequestTemplate template) {
-        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        if (attributes != null) {
+        ServletRequestAttributes attributes =
+                (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        if (attributes != null && attributes.getRequest() != null) {
             String token = attributes.getRequest().getHeader("Authorization");
-            if (token != null) {
+
+            if (token != null && !token.isEmpty()) {
+                if (!token.startsWith("Bearer ")) {
+                    token = "Bearer " + token;
+                }
                 template.header("Authorization", token);
             }
         }
