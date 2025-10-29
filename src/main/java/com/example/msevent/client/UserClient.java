7dbc8ac534097874cb9ad9c95575447762de1032
Aykhan Mammadli <mmmdliayxan2107@gmail.com>
2025-10-29 13:46:48 +0400

    add configuration

--- src/main/java/com/example/msevent/client/UserClient.java
+++ src/main/java/com/example/msevent/client/UserClient.java
@@ -1,11 +1,12 @@
 package com.example.msevent.client;
 
+import com.example.msevent.config.FeignAuthInterceptor;
 import com.example.msevent.dto.request.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "authClient", url = "http://ms-gateway:8080/api/users")
+@FeignClient(name = "authClient", url = "http://ms-gateway:8080/api/users", configuration = FeignAuthInterceptor.class)
 public interface UserClient {
     @GetMapping("/{id}")
     UserDto getUserById(@PathVariable("id") Long id);
--- src/main/java/com/example/msevent/config/FeignAuthInterceptor.java
+++ src/main/java/com/example/msevent/config/FeignAuthInterceptor.java
@@ -2,27 +2,20 @@ package com.example.msevent.config;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
-@Component
+@Configuration
 public class FeignAuthInterceptor implements RequestInterceptor {
 
     @Override
     public void apply(RequestTemplate template) {
-        ServletRequestAttributes attributes =
-                (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-
-        if (attributes != null && attributes.getRequest() != null) {
-            String token = attributes.getRequest().getHeader("Authorization");
-
-            if (token != null && !token.isEmpty()) {
-                if (!token.startsWith("Bearer ")) {
-                    token = "Bearer " + token;
-                }
-                template.header("Authorization", token);
-            }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getCredentials() != null) {
+            String token = authentication.getCredentials().toString();
+            template.header(HttpHeaders.AUTHORIZATION, "Bearer " + token);
         }
     }
 }
